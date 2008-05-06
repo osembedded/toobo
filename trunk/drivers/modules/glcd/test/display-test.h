3cbb4939ed412411afcb5cb649d02ac8df502cc6
osembedded <osembedded@aaa9ea3d-924a-0410-8c60-f9fd434fc42b>
2008-05-06 05:45:33 +0000

    initial revision. This is a test application written solely to test the glcd driver module.

---
+++ trunk/drivers/modules/glcd/test/display-test.h
@@ -0,0 +1,36 @@
+/**
+ * display-test.h [Part of the TOOBO Project]
+ * 
+ * Copyright 2008 Vijay Jayaraman (osembedded@gmail.com) 
+ * 
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU Lesser General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU Lesser General Public License 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef __DISPLAY_TEST_H__
+#define __DISPLAY_TEST_H__
+
+#define DBG(x...) printf(x)
+#define TRACE_IN  printf("\n%s(+):", __FUNCTION__)
+#define TRACE_OUT /* printf("\n%s(-):", __FUNCTION__)*/
+
+#ifndef SUCCESS
+#define SUCCESS 0
+#define FAILURE -1
+#endif
+
+// Turn this on to enable a small rectangle box where 
+// a single pixel moves around randomly. Just for fun...
+//#define TEST_LIFE_BOX
+
+#endif /* __DISPLAY_TEST_H__ */
