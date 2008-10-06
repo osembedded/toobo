bb041f42b425d2dbb9ab26da8f377644f754dbd4
osembedded <osembedded@aaa9ea3d-924a-0410-8c60-f9fd434fc42b>
2008-10-06 03:48:56 +0000

    More changes and new additions.

---
+++ app/src/origin/src/origin.common.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+##########################################################################
+# origin.common.sh [Part of the TOOBO project]
+# 
+# Copyright 2008 Vijay Jayaraman (osembedded@gmail.com) 
+# 
+# This program is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU Lesser General Public License as published by 
+# the Free Software Foundation, either version 3 of the License, or 
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+# GNU Lesser General Public License for more details. 
+# 
+# You should have received a copy of the GNU Lesser General Public License 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##########################################################################
+#
+# Common appilcation startup script for all Toobo models.
+# Usage -
+#         ./origin.common.sh
+#         [To be invoked from the top level origin.sh file]
+
+### Start of Functions ###
+### End of Functions ###
+
+### Start of 'code' ###
+echo ">]] origin.common.sh";
+
+
