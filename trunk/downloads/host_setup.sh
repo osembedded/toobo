92e32ad61fa9e4a64f280cf4d1452f189bf8c2b6
osembedded <osembedded@aaa9ea3d-924a-0410-8c60-f9fd434fc42b>
2009-07-05 06:17:12 +0000

    Updated and working.

--- trunk/downloads/host_setup.sh
+++ trunk/downloads/host_setup.sh
@@ -7,20 +7,26 @@
 TOP_LEVEL_DIR=..
 SCRIPTS_DIR=$TOP_LEVEL_DIR/scripts
 DOWNLOAD_DIR=$TOP_LEVEL_DIR/downloads
-KERNEL_SRC_DIR=$TOP_LEVEL_DIR/kernel
+KERNEL_SOURCES_DIR=$TOP_LEVEL_DIR/kernel
+PATCHES_DIR=$KERNEL_SOURCES_DIR/patches
 TOOLCHAIN_DIR=$TOP_LEVEL_DIR/toolchains
 
 ## Script names
 DOWNLOAD_SCRIPT=download_to_directory.sh
+PATCH_SCRIPT=patch_source.sh
+
+## Symbolic names
+SYM_LINK_KERNEL_SRC=linux-2.6
 
 ## URIs
 BASE_URI=http://toobo.googlecode.com/files
-KERNEL_ARCHIVE_NAME=tskernel-2.6.21-ts-src.tar.gz
-TOOLCHAIN_ARCHIVE_NAME=crosstool-linux26-3.4.4-2005q3-2.tar.gz
+TAR_GZ=.tar.gz
+KERNEL_ARCHIVE_NAME=linux-2.6.21-ts
+TOOLCHAIN_ARCHIVE_NAME=crosstool-linux26-3.4.4-2005q3-2
 
 ## Download the kernel source
 echo "Downloading the kernel source..."
-sh $SCRIPTS_DIR/$DOWNLOAD_SCRIPT $BASE_URI/$KERNEL_ARCHIVE_NAME $DOWNLOAD_DIR
+sh $SCRIPTS_DIR/$DOWNLOAD_SCRIPT $BASE_URI/$KERNEL_ARCHIVE_NAME$TAR_GZ $DOWNLOAD_DIR
 if [ $? -ne 0 ]
 then
     echo "Something went wrong while downloading the kernel source!"
@@ -29,16 +35,24 @@ fi
 
 ## Extract the kernel source to the appropriate directory
 echo "Extracting the Kernel source..."
-tar -xzf $KERNEL_ARCHIVE_NAME -C $KERNEL_SRC_DIR
+tar -xzf $KERNEL_ARCHIVE_NAME$TAR_GZ -C $KERNEL_SOURCES_DIR
 if [ $? -ne 0 ]
 then
     echo "Something went wrong while extracting the kernel source!"
     exit 1
 fi
 
+## Create a symlink to the kernel source
+echo "Creating symlink to kernel source"
+ln -s $KERNEL_SOURCES_DIR/$KERNEL_ARCHIVE_NAME $KERNEL_SOURCES_DIR/$SYM_LINK_KERNEL_SRC
+
+## Now patch the kernel if needed.
+echo "Patching the Kernel source..."
+sh $SCRIPTS_DIR/$PATCH_SCRIPT $KERNEL_SOURCES_DIR/$KERNEL_ARCHIVE_NAME $PATCHES_DIR/$KERNEL_ARCHIVE_NAME
+
 ## Download the toolchain
 echo "Downloading the toolchain source..."
-sh $SCRIPTS_DIR/$DOWNLOAD_SCRIPT $BASE_URI/$TOOLCHAIN_ARCHIVE_NAME $DOWNLOAD_DIR
+sh $SCRIPTS_DIR/$DOWNLOAD_SCRIPT $BASE_URI/$TOOLCHAIN_ARCHIVE_NAME$TAR_GZ $DOWNLOAD_DIR
 if [ $? -ne 0 ]
 then
     echo "Something went wrong while downloading the toolchain!"
@@ -47,7 +61,7 @@ fi
 
 echo "Extracting the Toolchain..."
 ## Extract the toolchain to the appropriate directory.
-tar -xzf $TOOLCHAIN_ARCHIVE_NAME -C $TOOLCHAIN_DIR
+tar -xzf $TOOLCHAIN_ARCHIVE_NAME$TAR_GZ -C $TOOLCHAIN_DIR
 
 if [ $? -ne 0 ]
 then
