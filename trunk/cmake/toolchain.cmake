f0e349a75562ac383dad0421a70eae8733b260ff
osembedded <osembedded@aaa9ea3d-924a-0410-8c60-f9fd434fc42b>
2009-10-03 21:53:27 +0000

    More cmake related changes. Inclues the toolchain configuration and paths cmake files and some test files.

---
+++ trunk/cmake/toolchain.cmake
@@ -0,0 +1,17 @@
+# this one is important
+SET ( CMAKE_SYSTEM_NAME Linux )
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 1)
+
+# specify the cross compiler
+SET ( CMAKE_C_COMPILER   ${TOOLCHAIN_DIR}/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-gcc )
+SET ( CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-g++ )
+
+# where is the target environment 
+SET ( CMAKE_FIND_ROOT_PATH  ${TARGET_ENV} )
+
+# search for programs in the build host directories
+SET ( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+# for libraries and headers in the target directories
+SET ( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+SET ( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
---
+++ trunk/tests/standalone/helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello from Toobo!\n");
+  return 0;
+}
